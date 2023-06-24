8323cfac4a0846252076a09f41a4113bc3f7caa5
Chaitrali Shinde <chai.shinde100@gmail.com>
2023-06-24 15:03:58 -0700

    model for a post defined

--- blogBuilder/theBlogApp/models.py
+++ blogBuilder/theBlogApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=500)
+    content=models.CharField()
+    author= models.CharField( max_length=100)
+    createdAt= models.DateTimeField(auto_now_add=False)
