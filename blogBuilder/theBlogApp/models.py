6f48a98bc3e04a0c86ef9cb12352b8fdf8b95a94
Chaitrali Shinde <chai.shinde100@gmail.com>
2023-06-26 21:12:06 -0700

    Model.py updated

--- blogBuilder/theBlogApp/models.py
+++ blogBuilder/theBlogApp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=500)
-    content=models.CharField()
+    content=models.TextField()
     author= models.CharField( max_length=100)
     createdAt= models.DateTimeField(auto_now_add=False)
--- blogBuilder/theBlogApp/views.py
+++ blogBuilder/theBlogApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+
+def allBlogPost(request):
+    blogPosts= Post.object.all()  #this query tell the DB to give all the objects
+    output= ",". join([p.title for p in blogPosts])
+    return HttpResponse(output)
