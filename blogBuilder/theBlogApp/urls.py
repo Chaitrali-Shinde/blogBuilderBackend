1b8ea2f2bb3c30f530bd245d0217177d956ed1be
Chaitrali Shinde <chai.shinde100@gmail.com>
2023-07-07 12:21:05 -0700

    rest_framework added

--- blogBuilder/theBlogApp/urls.py
+++ blogBuilder/theBlogApp/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path    
 from . import views
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet
+from django.http import JsonResponse
+
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
--- blogBuilder/theBlogApp/views.py
+++ blogBuilder/theBlogApp/views.py
@@ -1,23 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework import viewsets, status
 from .models import Post
-from django.http import JsonResponse
-from rest_framework import serializers, viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework import status
+from .serializers import PostSerializer
 
 # Create your views here.
-
 def allBlogPost(request):
-    blogPosts= Post.objects.all()  #this query tell the DB to give all the objects
-    output= ",". join([p.title for p in blogPosts])
-    return JsonResponse(output,  safe=False)
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['id','title', 'content', 'author','createdAt']
+    blogPosts = Post.objects.all()
+    serializer = PostSerializer(blogPosts, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
